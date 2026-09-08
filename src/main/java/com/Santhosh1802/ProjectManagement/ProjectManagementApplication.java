391ebd5b92a20a394c94f41b4430842cd14854b7
Santhosh1802 <71439086+Santhosh1802@users.noreply.github.com>
2026-09-08 23:39:58 +0530

    Initial commit

---
+++ src/main/java/com/Santhosh1802/ProjectManagement/ProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Santhosh1802.ProjectManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Santhosh1802/ProjectManagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Santhosh1802.ProjectManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
