a6c3f325f218c25a9b80048489b63feaae7d1737
Santhosh1802 <71439086+Santhosh1802@users.noreply.github.com>
2026-09-18 20:59:30 +0530

    V1 removed ProjectRepository for unused variables and imports

--- src/main/java/com/Santhosh1802/ProjectManagement/repository/ProjectRepository.java
+++ src/main/java/com/Santhosh1802/ProjectManagement/repository/ProjectRepository.java
@@ -1,13 +1,16 @@
 package com.Santhosh1802.ProjectManagement.repository;
 
 import com.Santhosh1802.ProjectManagement.entity.Project;
+import com.Santhosh1802.ProjectManagement.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, UUID> {
-    public Optional<Project> findProjectByName(String name);
+    Optional<Project> findProjectByName(String name);
+    List<Project> findProjectsByOwner(User user);
 }
